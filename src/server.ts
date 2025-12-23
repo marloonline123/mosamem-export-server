3bb4665475dbb04879d00897dd757c65b7efeafe
Abotalib Adam <abotalib@abotalib.com>
2025-12-23 20:22:53 +0200

    feat: update project configuration for module support and environment variable management

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import exportRoutes from './routes/export';
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
